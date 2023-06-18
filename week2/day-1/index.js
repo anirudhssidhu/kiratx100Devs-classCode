1b6b54f0cf173844db638eca3ef3074a467c25e5
anirudhssidhu <anirudhssidhu@gmail.com>
2023-06-18 20:46:30 +0530

    usint fetch and then response body

--- week2/day-1/index.js
+++ week2/day-1/index.js
@@ -4,13 +4,6 @@ var bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-// function middleware1(req, res, next) {
-//   console.log(`from inside middleware1 \n${req.body}`);
-//   // res.send("This is by using respond and not request!");
-//   next();
-// }
-
-// app.use(middleware1);
 app.use(bodyParser.json());
 
 function calculateSum(counter) {
@@ -21,30 +14,17 @@ function calculateSum(counter) {
   return sum;
 }
 
-function calculateFactorial(counter) {
-  let prod = 1;
-  for (var i = 1; i <= counter; i++) {
-    prod *= i;
-  }
-  return prod;
-}
 function handleSum(req, res) {
-  // const counter = req.headers.counter;
-  // const counter = req.query.counter;
-
-  counter = req.body.counter;
-
+  counter = req.query.counter;
   let calculatedSum = calculateSum(counter);
-  let calculatedMul = calculateFactorial(counter);
   var ansObject = {
     sum: calculatedSum,
-    factorial: calculatedMul,
   };
   res.status(200).send(ansObject);
 }
 
 function givePage(req, res) {
-res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html");
 }
 
 function sayHello(req, res) {
@@ -54,19 +34,9 @@ function sayHello(req, res) {
   res.send(`Hello ${name}, Welcome to our website!`);
 }
 
-function createUser(req, res) {
-  res.send("creating user.........");
-}
-
-function deleteUser(req, res) {
-  res.send("deleting user .........");
-}
-
 app.get("/hello", sayHello);
 app.get("/", givePage);
 app.get("/handleSum", handleSum);
-app.post("/createUser", createUser);
-app.delete("/deleteUser", deleteUser);
 
 function started() {
   console.log(`Example app listening on port ${port}.`);
